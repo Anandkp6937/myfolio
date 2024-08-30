525618cf2b500f633fb03e73036c5dc9c1cd7d15
anandkp6937 <anandkp6937@gmail.com>
2024-08-30 16:17:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const navLinks=document.querySelector('.links');
+const logo=document.querySelector('.logo');
+const connectButton=document.querySelector('.btnC');
+const modalBorder=document.querySelector('.section4');
+document.addEventListener('DOMContentLoaded',()=>{
+  setTimeout(()=>{
+    navLinks.style.transform="rotateZ(360deg)";
+  },1000);
+ 
+})
+connectButton.addEventListener('click',()=>{
+  modalBorder.classList.add('borderModal');
+  setTimeout(()=>{
+    modalBorder.classList.remove('borderModal')
+  },1000);
+
+})
