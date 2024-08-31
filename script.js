9b47f82e5407011910a6c26e67701ffa6b7f6fac
anandkp6937 <anandkp6937@gmail.com>
2024-08-31 17:33:49 +0530

    responsive edit and added animations

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const navLinks=document.querySelector('.links');
 const logo=document.querySelector('.logo');
 const connectButton=document.querySelector('.btnC');
 const modalBorder=document.querySelector('.section4');
+
 document.addEventListener('DOMContentLoaded',()=>{
   setTimeout(()=>{
     navLinks.style.transform="rotateZ(360deg)";
