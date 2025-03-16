51a8c62c2534912ecbe51f93d5237cff6d8ee768
anandkp6937 <anandkp6937@gmail.com>
2025-03-16 16:18:36 +0530

    minor changes ;

--- script.js
+++ script.js
@@ -8,20 +8,3 @@ connectBtn.addEventListener('click',()=>{
   },500)
   
 })
-window.addEventListener('DOMContentLoaded',async()=>{
-  let data=await fetch('https://random-rho-nine.vercel.app/blog');
-  let blogData=await data.json();
-  await makeCards(blogData);
-  
-})
-function makeCards(arr){
-  blogContainer.innerHTML='';
-  arr.forEach((item)=>{
-    blogContainer.innerHTML +=`<div class="blog-comp">
-                <h2 class="blog-link blg"><a href="https://${item.url}">${item.title}</a></h2>
-                <div class=""><h2 class="blog-desc blg">${item.description}</h2></div>
-            </div>`
-
-  })
-
-}
