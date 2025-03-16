8ff6d8ae3b26b3d3fa81625a18aa8ca51a2948c4
anandkp6937 <anandkp6937@gmail.com>
2025-03-16 16:02:54 +0530

    modified ui elements added blog section

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const connectBtn=document.querySelector('.btnC');
 const links=document.getElementById('connectUl');
+const blogContainer=document.querySelector('.blog-wrap');
 connectBtn.addEventListener('click',()=>{
   links.style.transform='scale(0.8)';
   setTimeout(()=>{
@@ -7,3 +8,20 @@ connectBtn.addEventListener('click',()=>{
   },500)
   
 })
+window.addEventListener('DOMContentLoaded',async()=>{
+  let data=await fetch('https://random-rho-nine.vercel.app/blog');
+  let blogData=await data.json();
+  await makeCards(blogData);
+  
+})
+function makeCards(arr){
+  blogContainer.innerHTML='';
+  arr.forEach((item)=>{
+    blogContainer.innerHTML +=`<div class="blog-comp">
+                <h2 class="blog-link blg"><a href="https://${item.url}">${item.title}</a></h2>
+                <div class=""><h2 class="blog-desc blg">${item.description}</h2></div>
+            </div>`
+
+  })
+
+}
